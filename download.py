aa6eca1e8954751f6f9921644d2b8b9b7805c346
Veniashvilly <151940586+Veniashvilly@users.noreply.github.com>
2025-05-29 18:47:22 +0500

    download.py

--- download.py
+++ download.py
@@ -2,44 +2,26 @@ import pandas as pd
 from sklearn.preprocessing import OrdinalEncoder
 
 def download_data():
-    df = pd.read_csv('https://raw.githubusercontent.com/Alexey3250/Start_ML/refs/heads/main/Machine_learning/10_classification/banking.csv', delimiter = ',')
+    df = pd.read_csv("https://raw.githubusercontent.com/Veniashvilly/dtset/refs/heads/main/Car_sales.csv")
     return df
 
 def clear_data():
-    df = pd.read_csv('https://raw.githubusercontent.com/Alexey3250/Start_ML/refs/heads/main/Machine_learning/10_classification/banking.csv', delimiter = ',')
+    df = download_data()
 
-    ### Уберем колонку duration
+    # Оставим только нужные колонки и удалим строки с пропущенными значениями
+    df = df[[
+        'Manufacturer', 'Model', 'Vehicle_type',
+        'Sales_in_thousands', 'Engine_size', 'Horsepower',
+        'Curb_weight', 'Fuel_efficiency', 'Price_in_thousands'
+    ]].dropna().reset_index(drop=True)
 
-    df = df.drop('duration', axis=1)
-    ### Посмотрим на категориальные колонки
+    # Закодируем категориальные признаки
+    cat_columns = ['Manufacturer', 'Model', 'Vehicle_type']
+    encoder = OrdinalEncoder()
+    df[cat_columns] = encoder.fit_transform(df[cat_columns])
 
-    categorical_columns = df.loc[:,df.dtypes=='object'].columns
+    # Сохраним очищенные данные
+    df.to_csv("df_clear.csv", index=False)
 
-    df = df.drop(['loan', 'housing', 'marital'], axis=1)
-    categorical_columns = categorical_columns.drop(['loan', 'housing', 'marital'])
-
-    for col in categorical_columns:
-    
-        ### К колонкам с маленькой размерностью применим one-hot
-        if df[col].nunique() < 5:
-            one_hot = pd.get_dummies(df[col], prefix=col, drop_first=True).astype(int)
-            df = pd.concat((df.drop(col, axis=1), one_hot), axis=1)
-            
-        ### К остальным - счетчики
-        else:
-            mean_target = df.groupby(col)['y'].mean()
-            df[col] = df[col].map(mean_target)
-    
-    categorical_columns = df.loc[:,df.dtypes=='object'].columns
-    
-    df = df.reset_index(drop=True)  
-    ordinal = OrdinalEncoder()
-    ordinal.fit(df[categorical_columns])
-    Ordinal_encoded = ordinal.transform(df[categorical_columns])
-    df_ordinal = pd.DataFrame(Ordinal_encoded, columns=categorical_columns)
-    df[categorical_columns] = df_ordinal[categorical_columns]
-    df.to_csv('df_clear.csv')
-    return True
-
-download_data()
-clear_data()
+if __name__ == "__main__":
+    clear_data()
